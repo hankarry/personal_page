ced222373fdc9604bf54db2ae047d18f421115e8
han-karry <2142865852@qq.com>
2022-05-20 12:05:01 +0800

    终于解决按钮的问题了

--- js/connect.js
+++ js/connect.js
@@ -1,18 +1,24 @@
-var x = 0
-function showhide() {
-    if (x == 0) {
 
-        document.getElementById('server_detail_mumble').style.display="block";
-        // document.getElementById('server_detail_mumble').style.animationName = "hidefrom";
-        document.getElementById('server_detail_mumble').style.animationName = "showfrom_top";
-        x = x + 1;
-    }
+function showhide(button, detail, text) {
+    if (document.getElementById(detail).style.display == "block") {
 
-    if (x == 1) {
+        document.getElementById(detail).style.animationName = "hidefrom";
+        document.getElementById(text).style.animationName = "hidefrom_text";
+        document.getElementById(text).style.animationDelay = "0s";
         
-        document.getElementById('server_detail_mumble').style.display="none";
-        document.getElementById('server_detail_mumble').style.animationName = "hidefrom";
-        x = x - 1;
+
+        document.getElementById(button).style.animationName = "button_360";
+
+        //setTimeout的第一个参数必须是需要编译的代码或者是一个函数方法，而如果直接传入一行可执行代码，那么抱歉，这里会立即执行，没有延迟效果。
+        setTimeout (function() {document.getElementById(detail).style.display='none'}, 2000)
+        
+    }
+    else {
+    document.getElementById(text).style.animationName = "showfrom_top_text";
+    document.getElementById(text).style.animationDelay = "1s";
+    document.getElementById(detail).style.display = "block";
+    document.getElementById(detail).style.animationName = "showfrom_top";
+    
+    document.getElementById(button).style.animationName = "button_180";
     }
 }
-document.getElementById('server_detail_mumble').addEventListener("click", showhide);
