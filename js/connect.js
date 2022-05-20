bc28eebd0df476fd6c5c070a52fc59d3d97a44f5
han-karry <2142865852@qq.com>
2022-05-20 19:28:17 +0800

    下拉div

--- js/connect.js
+++ js/connect.js
@@ -10,12 +10,12 @@ function showhide(button, detail, text) {
         document.getElementById(button).style.animationName = "button_360";
 
         //setTimeout的第一个参数必须是需要编译的代码或者是一个函数方法，而如果直接传入一行可执行代码，那么抱歉，这里会立即执行，没有延迟效果。
-        setTimeout (function() {document.getElementById(detail).style.display='none'}, 2000)
+        setTimeout (function() {document.getElementById(detail).style.display='none'}, 500)
         
     }
     else {
     document.getElementById(text).style.animationName = "showfrom_top_text";
-    document.getElementById(text).style.animationDelay = "1s";
+    document.getElementById(text).style.animationDelay = "0.5s";
     document.getElementById(detail).style.display = "block";
     document.getElementById(detail).style.animationName = "showfrom_top";
     
