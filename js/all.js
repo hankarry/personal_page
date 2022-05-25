5d542518e430816d110d7b2905f5f5a27d3294cf
han-karry <2142865852@qq.com>
2022-05-25 19:42:27 +0800

    页面制作

---
+++ js/all.js
@@ -0,0 +1,7 @@
+setTimeout (function() {
+    document.getElementById('message_warn').style.animationName = 'message_out';
+    setTimeout (function() {
+        document.getElementById('message_warn').style.display = 'none';
+    }, 1000)
+}, 10000)
+        
