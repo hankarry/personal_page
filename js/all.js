b77fd76219d9bea22bd24cb0f2a7c247f70ea9bd
han-karry <2142865852@qq.com>
2022-05-26 21:21:18 +0800

    目标界面制作,新页面规划

--- js/all.js
+++ js/all.js
@@ -1,7 +1,7 @@
-setTimeout (function() {
-    document.getElementById('message_warn').style.animationName = 'message_out';
-    setTimeout (function() {
-        document.getElementById('message_warn').style.display = 'none';
-    }, 1000)
-}, 10000)
+// setTimeout (function() {
+//     document.getElementById('message_warn').style.animationName = 'message_out';
+//     setTimeout (function() {
+//         document.getElementById('message_warn').style.display = 'none';
+//     }, 1000)
+// }, 10000)
         
---
+++ js/goal.js
@@ -0,0 +1,9 @@
+// window.onload = function() {
+
+//     
+// }
+
+for (i = 1; i = 4; i++) {
+
+    document.getElementById('div_card').getElementsByTagName('div').style.animationDelay = String(i) + 's';
+}
