23dabf5f1b7508df045bf1c2b0aa07e725a26a05
寒-karry <2142865852@qq.com>
2022-06-22 22:36:24 +0800

    生日取消

--- js/all.js
+++ js/all.js
@@ -25,22 +25,22 @@
 // AddSheetFile("css/header.css");
 // AddSheetFile("css/all.css");
 
-var d = new Date;
-var birthday_count = 19 - d.getDate();
+// var d = new Date;
+// var birthday_count = 19 - d.getDate();
 // var birthday = 19 - day;
 // var birthday_count = 19 - day;
 // console.log (day)
-if (birthday_count == 0) {
-    birthday_text = "今天是咱的生日!"
-}
-if (birthday_count > 0) {
-    birthday_text = "还有" + String(birthday_count) + "天是咱生日哦~"
-}
-if (birthday_count < 0) {
-    birthday_text = ""
-}
+// if (birthday_count == 0) {
+//     birthday_text = "今天是咱的生日!"
+// }
+// if (birthday_count > 0) {
+//     birthday_text = "还有" + String(birthday_count) + "天是咱生日哦~"
+// }
+// if (birthday_count < 0) {
+//     birthday_text = ""
+// }
 // document.write = "<div id='top_message'><img src='/css/hud/cake.svg' width='25' height='25'><section>咱的生日快到啦</section></div>"
 var newelement = document.createElement("div");
 newelement.id = "top_message";
 document.getElementById("body").insertBefore(newelement, document.getElementById("body").firstChild)
-document.getElementById("top_message").innerHTML = "<img src='/css/hud/cake.svg' width='25' height='25'><section>" + birthday_text + "🎉</section>"
+document.getElementById("top_message").innerHTML = "<section></section>"
