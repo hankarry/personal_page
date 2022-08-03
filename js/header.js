c22f21961479c138a0944a03835e488d0b30718c
寒-karry <2142865852@qq.com>
2022-08-03 22:36:49 +0800

    新增导航栏伸缩

--- js/header.js
+++ js/header.js
@@ -29,6 +29,72 @@ AddSheetFile("/css/header_mode_2.css");
 // document.createElement('div').id = 'header'
 // AddSheetFile("css/all.css");
 
+// // 导航栏数量,当前导航栏1的id,当前导航栏2的id,导航栏2的html
+// function Headers(number, now_page1, now_page2, header2_gui, transparent) {
+//     header_number = number
+//     // document.write("<div id='header'></div>")
+//     var header = document.createElement("div");
+//     header.id = "div_header";
+//     // header.innerHTML = "<div id='header'></div>";
+//     // document.getElementsByTagName('body')[0].insertBefore(header, document.body.childNodes[2]);
+//     document.getElementById("body").insertBefore(header, document.getElementById("body").childNodes[2]);
+//     var xhttp
+//     xhttp = new XMLHttpRequest();
+//     xhttp.onreadystatechange = function () {
+//         if (this.readyState == 4 && this.status == 200) {
+//             document.getElementById("div_header").innerHTML = this.responseText;
+//             // document.getElementsByTagName('body')[0].write = this.responseText
+//             // document.write = this.responseText
+//         }
+//     };
+//     xhttp.open("GET", "/header/header.html", false)
+//     xhttp.send()
+
+//     if (transparent == 'true') {
+//         AddSheetFile("/css/header_transparent.css")
+//     } else {
+//         AddSheetFile("/css/header.css")
+//     }
+
+//     if (number == 2) {
+//         // document.getElementsByTagName('body')[0].write = "<script src = 'js/header2.js'></script>";
+//         // var script = document.createElement('script');
+//         // script.src = 'js/header2.js';
+//         // document.getElementsByTagName('body')[0].appendChild(script);
+//         // script.innerHTML = 'getheader2(' + String(header2_gui) +');heade2_backgroundset(' + String(now_page2) + ')'
+
+        
+//             // if (header2_gui == "me") {
+        
+//             //     page_request("header2_me.html")
+//             // }
+//         AddSheetFile("/css/header2.css")
+        
+//         var xhttp
+//         xhttp = new XMLHttpRequest();
+//         xhttp.onreadystatechange = function () {
+//             if (this.readyState == 4 && this.status == 200) {
+//                 document.getElementById("header2").innerHTML = this.responseText;
+//                 // document.getElementsByTagName('body')[0].write = this.responseText
+//                 // document.write = this.responseText
+//             }
+//         };
+//         xhttp.open("GET", header2_gui, false)
+//         xhttp.send()
+//         document.getElementById(now_page2).style.backgroundColor = "var(--backgroundcolor)"
+        
+//         document.getElementById(now_page2).onclick = "unset"
+//     }
+    
+//     document.getElementById(now_page1).onclick = "unset"
+
+//     // document.getElementById(now_page1).style.backgroundColor = "var(--backgroundcolor)";
+//     document.getElementById(now_page1).style.border = "1px solid black";
+
+//     // document.getElementById(now_page1).style.borderBottom = "unset"
+
+// }
+
 // 导航栏数量,当前导航栏1的id,当前导航栏2的id,导航栏2的html
 function Headers(number, now_page1, now_page2, header2_gui, transparent) {
     header_number = number
@@ -50,46 +116,23 @@ function Headers(number, now_page1, now_page2, header2_gui, transparent) {
     xhttp.open("GET", "/header/header.html", false)
     xhttp.send()
 
-    document.getElementById(now_page1).onclick = "unset"
+    var page1 = document.getElementById(now_page1);
+    page1.onclick = "unset";
 
-    document.getElementById(now_page1).style.backgroundColor = "var(--backgroundcolor)"
+    // document.getElementById(now_page1).style.backgroundColor = "var(--backgroundcolor)";
+    page1.style.borderWidth = "2px";
+    page1.style.borderRadius = "30px 0 0 30px"
+    
+    if (now_page2) {
+        var page2 = document.getElementById(now_page2);
+        page2.onclick = "unset";
+        page2.style.borderWidth = "1px";
+        page2.style.backgroundColor = "var(--maincolor_thin3)"
+        page2.style.borderRadius = "20px 0 0 20px"    
+    }
+
     // document.getElementById(now_page1).style.borderBottom = "unset"
 
-    if (transparent == 'true') {
-        AddSheetFile("/css/header_transparent.css")
-    } else {
-        AddSheetFile("/css/header.css")
-    }
-
-    if (number == 2) {
-        // document.getElementsByTagName('body')[0].write = "<script src = 'js/header2.js'></script>";
-        // var script = document.createElement('script');
-        // script.src = 'js/header2.js';
-        // document.getElementsByTagName('body')[0].appendChild(script);
-        // script.innerHTML = 'getheader2(' + String(header2_gui) +');heade2_backgroundset(' + String(now_page2) + ')'
-
-        
-            // if (header2_gui == "me") {
-        
-            //     page_request("header2_me.html")
-            // }
-        AddSheetFile("/css/header2.css")
-        
-        var xhttp
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("header2").innerHTML = this.responseText;
-                // document.getElementsByTagName('body')[0].write = this.responseText
-                // document.write = this.responseText
-            }
-        };
-        xhttp.open("GET", header2_gui, false)
-        xhttp.send()
-        document.getElementById(now_page2).style.backgroundColor = "var(--backgroundcolor)"
-        
-        document.getElementById(now_page2).onclick = "unset"
-    }
 }
 
 function jumpto(herf) {
@@ -109,40 +152,74 @@ function jumpto(herf) {
     }, 500)
 }
 
-function header_changemode() {
+// function header_changemode() {
     
-    var header = document.getElementById("header");
-    // var header2 = document.getElementById("div_header2")
-    header.style.animation = "header1_mode1_hide 1.5s ease 0.3s 1 normal both";
-    // header2.style.animation = "header2_mode1_hide 1.5s ease 0s 1 normal both";
-    setTimeout(function() {
-        // header.style.display = "none"
-        // header2.style.display = "none"
-    }, 2000)
+//     var header = document.getElementById("header");
+//     // var header2 = document.getElementById("div_header2")
+//     header.style.animation = "header1_mode1_hide 1.5s ease 0.3s 1 normal both";
+//     // header2.style.animation = "header2_mode1_hide 1.5s ease 0s 1 normal both";
+//     setTimeout(function() {
+//         // header.style.display = "none"
+//         // header2.style.display = "none"
+//     }, 2000)
 
-    if (header_number == 2) {
-        var header2 = document.getElementById("div_header2")
-        // header.style.animation = "header1_mode1_hide 1s ease 1s 1 normal both";
-        header2.style.animation = "header2_mode1_hide 0.5s ease 0s 1 normal both";
-        setTimeout(function() {
-            // header.style.display = "none"
-            header2.style.display = "none";
+//     if (header_number == 2) {
+//         var header2 = document.getElementById("div_header2")
+//         // header.style.animation = "header1_mode1_hide 1s ease 1s 1 normal both";
+//         header2.style.animation = "header2_mode1_hide 0.5s ease 0s 1 normal both";
+//         setTimeout(function() {
+//             // header.style.display = "none"
+//             header2.style.display = "none";
 
-        }, 500)
-    }
+//         }, 500)
+//     }
 
-    setTimeout(function() {
+//     setTimeout(function() {
         
-        document.getElementById("header_mode2").style.display = "block";
-        document.getElementById("body_right").style.animation = "card_move_right 1s ease 0s 1 normal both";
-    }, 2000)
+//         document.getElementById("header_mode2").style.display = "block";
+//         document.getElementById("body_right").style.animation = "card_move_right 1s ease 0s 1 normal both";
+//     }, 2000)
 
-    document.cookie = "header_mode=2; path=/"
-}
+//     document.cookie = "header_mode=2; path=/"
+// }
 
-if (document.cookie == "header_mode=2") {
-    setTimeout(function() {
-        header_changemode()
+// if (document.cookie == "header_mode=2") {
+//     setTimeout(function() {
+//         header_changemode()
 
-    }, 1000)
+//     }, 1000)
+// }
+
+function ChangeHeaderWidth() {
+    var header = document.getElementById("header_mode2")
+    var root = document.documentElement
+    var button = document.getElementById("flex")
+    // console.log(getComputedStyle(document.documentElement).getPropertyValue('--header_mode2_width'))
+    var headerw = getComputedStyle(document.documentElement).getPropertyValue('--header_mode2_width')
+    // console.log(headerw)
+
+    if (headerw == "110px") {
+        // console.log("y")
+        header.style.width = "60px"
+        root.style.setProperty('--header_mode2_width', '60px')
+        // button.onclick = "ChangeHeaderWidth('60px')"
+        button.style.animationName = "button_180"
+
+        var header2 = document.getElementsByClassName("header2_mode2")
+        for (i = 0; i < header2.length; i++) {
+            header2[i].style.maxHeight = "0px"
+        }
+    }
+    else
+    if (headerw == "60px") {
+        header.style.width = "110px"
+        root.style.setProperty('--header_mode2_width', '110px')
+        // button.onclick = "ChangeHeaderWidth('110px')"
+        button.style.animationName = "button_360"
+        
+        var header2 = document.getElementsByClassName("header2_mode2")
+        for (i = 0; i < header2.length; i++) {
+            header2[i].style.maxHeight = "300px"
+        }
+    }
 }
--- js/loading.js
+++ js/loading.js
@@ -9,48 +9,36 @@
 // }
 // AddSheetFile("/css/loading.css");
 
-var anim = document.createElement("style")
-anim.innerHTML = "@keyframes loading_show {0% {opacity: 0;}100% {background-image: linear-gradient(to top, #30cfd0 0%, #330867 100%); opacity: 1;}}@keyframes loading_hide {0% {background-image: linear-gradient(to top, #30cfd0 0%, #330867 100%); opacity: 1;}100% {opacity: 0;}}@keyframes loading_font_show {0% {font-size: 8vw;}25% {font-size: 12vw;}50% {font-size: 9.75vw;}70% {font-size: 10.25vw;}100% {font-size: 10vw;}}@keyframes loading_font_hide {0% {font-size: 10vw;}25% {font-size: 10.25vw;}50% {font-size: 9.75vw;}70% {font-size: 12vw;}100% {font-size: 8vw;}}"
-document.head.appendChild(anim)
+// var anim = document.createElement("style")
+// anim.innerHTML = "@keyframes loading_show {0% {opacity: 0;}100% {background-image: linear-gradient(to top, #30cfd0 0%, #330867 100%); opacity: 1;}}@keyframes loading_hide {0% {background-image: linear-gradient(to top, #30cfd0 0%, #330867 100%); opacity: 1;}100% {opacity: 0;}}@keyframes loading_font_show {0% {font-size: 8vw;}25% {font-size: 12vw;}50% {font-size: 9.75vw;}70% {font-size: 10.25vw;}100% {font-size: 10vw;}}@keyframes loading_font_hide {0% {font-size: 10vw;}25% {font-size: 10.25vw;}50% {font-size: 9.75vw;}70% {font-size: 12vw;}100% {font-size: 8vw;}}"
+// document.head.appendChild(anim)
 
-var create = document.createElement("div");
-create.id = "loading";
-create.style = "position: fixed;z-index: 14;top: 0;width: 100vw;height: 100vh;animation-name: loading_show;animation-fill-mode: both;animation-duration: 1s;animation-delay: 0s;animation-timing-function: ease;"
-document.body.insertBefore(create, document.body.childNodes[3]);
-var create_child = document.createElement("section")
-create_child.id = "loading_section"
-create_child.style = "position: absolute;width: 90vw;color: rgb(80, 153, 198);margin: 40vh auto 40vh auto;font-size: 8vw;text-align: center;animation: loading_font_show 1s ease 0s 1 normal both;"
-document.getElementById("loading").appendChild(create_child)
-document.getElementById("loading_section").innerHTML = "Loading..."
+// var create = document.createElement("div");
+// create.id = "loading";
+// create.style = "position: fixed;z-index: 14;top: 0;width: 100vw;height: 100vh;animation-name: loading_show;animation-fill-mode: both;animation-duration: 1s;animation-delay: 0s;animation-timing-function: ease;"
+// document.body.insertBefore(create, document.body.childNodes[3]);
+// var create_child = document.createElement("section")
+// create_child.id = "loading_section"
+// create_child.style = "position: absolute;width: 90vw;color: rgb(80, 153, 198);margin: 40vh auto 40vh auto;font-size: 8vw;text-align: center;animation: loading_font_show 1s ease 0s 1 normal both;"
+// document.getElementById("loading").appendChild(create_child)
+// document.getElementById("loading_section").innerHTML = "Loading..."
 
+// setTimeout (function(){
+//     var load = document.getElementById("loading").style;
+//     // load.animation = "loading_show 1s ease 0s 1 reverse both";
+//     load.animationName = "loading_hide";
+//     load.animationDelay = "0.5s";
+//     // load.animationDirection = "reverse"
+//     // load.animationDuration = "1s"
+//     var load_section = document.getElementById("loading").lastChild.style;
+//     load_section.animationName = "loading_font_hide";
 
-// document.getElementById("loading").innerHTML = "<section>loading...</section>"
-
-// document.onreadystatechange = function() {
-//     if (document.readyState == "complete") {
-//         document.getElementById("loading").style.animation = "loading_show 1s ease 0s 1 reverse both"
-//         document.getElementById("loading").lastChild.style.animation = "loading_font_show 1s ease 0s 1 reverse both"
-//         setTimeout (function(){
-//             document.getElementById("loading").style.display = "none";
-//         }, 2000)
-//     }
-// }
-setTimeout (function(){
-    var load = document.getElementById("loading").style;
-    // load.animation = "loading_show 1s ease 0s 1 reverse both";
-    load.animationName = "loading_hide";
-    load.animationDelay = "0.5s";
-    // load.animationDirection = "reverse"
-    // load.animationDuration = "1s"
-    var load_section = document.getElementById("loading").lastChild.style;
-    load_section.animationName = "loading_font_hide";
-
-    // document.getElementById("loading").lastChild.style.animation = "loading_font_show 1s ease 0s 1 reverse both";
-    // document.getElementById("loading").lastChild.style.animationDirection = "reverse"
-}, 2000)
+//     // document.getElementById("loading").lastChild.style.animation = "loading_font_show 1s ease 0s 1 reverse both";
+//     // document.getElementById("loading").lastChild.style.animationDirection = "reverse"
+// }, 2000)
 setTimeout (function(){
     document.getElementById("body").style.display = "block";
-}, 2000)
-setTimeout (function(){
-    document.getElementById("loading").style.display = "none";
-}, 3000)
+}, 1000)
+// setTimeout (function(){
+//     document.getElementById("loading").style.display = "none";
+// }, 3000)
