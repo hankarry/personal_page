a41c70c959dc2de69b992ca65920b0899fdb3947
寒karry <2142865852@qq.com>
2022-10-08 22:05:36 +0800

    主页制作

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 setTimeout(function(){
-    document.getElementById("body").style.display = "block";
+    document.getElementById("card_1").style.animationPlayState = "running";
 }, 3000)
 
 setTimeout(function(){
@@ -46,14 +46,21 @@ for (var i=0; i< my_goals.length; i++) {
     my_goals_already_show.push(0)
 }
 
+var my_friends_showstate = 0;
+var my_goals_already_show_completely = 0;
+var title_text_completely = 0;
+
 window.onscroll = function() {
     var scroll_height = Html.scrollTop + window_height;
     // console.log(scroll_height);
-    if (scroll_height - 100 > my_friends.offsetTop) {
-        for (var i=0; i<my_friends_div.length; i++) {
-            // my_friends_div[i].style.visibility = "visible";
-            my_friends_div[i].style.animationName = "my_friends_div_show";
-            my_friends_div[i].style.animationDelay = 0.2 * i + "s";
+    if (my_friends_showstate == 0) {
+        if (scroll_height - 100 > my_friends.offsetTop) {
+            for (var i=0; i<my_friends_div.length; i++) {
+                // my_friends_div[i].style.visibility = "visible";
+                my_friends_div[i].style.animationName = "my_friends_div_show";
+                my_friends_div[i].style.animationDelay = 0.2 * i + "s";
+            }
+            my_friends_showstate = 1;
         }
     }
 
@@ -63,19 +70,29 @@ window.onscroll = function() {
         scroll_bottom_sign = 1;
     }
 
-    for (var i=0; i<title_text.length; i++) {
-        if (scroll_height - 100 > title_text[i].offsetTop) {
-            title_text[i].style.animationName = "title_text";
+    if (title_text_completely == 0) {
+        for (var i=0; i<title_text.length; i++) {
+            if (scroll_height - 100 > title_text[i].offsetTop) {
+                title_text[i].style.animationName = "title_text";
+                if (i == title_text.length - 1) {
+                    title_text_completely == 1;
+                }
+            }
         }
     }
 
-    for (var i=0; i< my_goals.length; i++) {
-        if (my_goals_already_show[i] == 0) {
-            if (scroll_height - 100 > my_goals[i].offsetTop) {
-                my_goals[i].style.animationName = "my_goals";
-                my_goals_already_show[i] = 1;
+    if (my_goals_already_show_completely == 0) {
+        
+        for (var i=0; i< my_goals.length; i++) {
+            if (my_goals_already_show[i] == 0) {
+                if (scroll_height - 100 > my_goals[i].offsetTop) {
+                    my_goals[i].style.animationName = "my_goals";
+                    my_goals_already_show[i] = 1;
+                    if (i == my_goals.length - 1) {
+                        my_goals_already_show_completely == 1;
+                    }
+                }
             }
-            
         }
     }
 
@@ -88,7 +105,7 @@ var gaokao_date = new Date("2024-06-07 09:00:00");
 var gaokao_time = parseInt(gaokao_date.getTime());
 // console.log(gaokao_date.getTime())
 var gaokao_countdown_ele = document.getElementById("gaokao_countdown");
-function gaokao_countdown() {
+function gaokao_countdown_time() {
     var now_date = new Date();
 
     var leftTime = gaokao_time - parseInt(now_date.getTime())
@@ -101,12 +118,12 @@ function gaokao_countdown() {
     var seconds = Math.floor((leftTime - days * 86400000 - hours * 3600000 - minutes * 60000) / 1000);
     // document.getElementById("gaokao_countdown").innerText = years + "年" + months + "月" + days + "日" + hours + "时" + minutes + "分" + seconds + "秒"; 
     gaokao_countdown_ele.innerHTML = days + "天" + hours + "小时" + minutes + "分" + seconds + "秒"; 
-    // setTimeout(gaokao_countdown(), 1000)
+    setTimeout(gaokao_countdown_time, 1000)
     // console.log(1)
-    setTimeout(gaokao_countdown, 1000)
+    // setTimeout(gaokao_countdown, 1000)
 
 }
-gaokao_countdown()
+gaokao_countdown_time()
 
 function Add_Copy(ele, conetnt) {
     navigator.clipboard.writeText(conetnt);
